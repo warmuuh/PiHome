6eb280ebb3468c60811e2d5d0f02e2aea4318d4f
ptmucha@gmail.com <>
2014-07-21 13:22:41 +0000

    init

---
+++ src/wrm/pihome/Greeting.java
@@ -0,0 +1,25 @@
+package wrm.pihome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Greeting extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    public void onClick(View view){
+      EditText input = (EditText) findViewById(R.id.host);
+      String string = input.getText().toString();
+      Toast.makeText(this, "Button1 pressed:" + string, Toast.LENGTH_LONG).show();
+    }
+}
