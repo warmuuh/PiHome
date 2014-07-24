c8ce0912490918ac71a46e9e08daeef423dede43
pmucha <peter.mucha@affinitas.de>
2014-07-24 14:36:45 +0200

    added first tasker integration

--- gen/wrm/pihome/R.java
+++ gen/wrm/pihome/R.java
@@ -13,38 +13,74 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f090000;
+        public static final int activity_vertical_margin=0x7f090001;
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
         public static final int bg_pattern=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int twofortyfouram_locale_ic_menu_dontsave=0x7f020003;
+        public static final int twofortyfouram_locale_ic_menu_help=0x7f020004;
+        public static final int twofortyfouram_locale_ic_menu_save=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int cb_switch1=0x7f090002;
-        public static final int cb_switch2=0x7f090003;
-        public static final int cb_switch3=0x7f090004;
-        public static final int cb_switch4=0x7f090005;
-        public static final int container=0x7f090000;
-        public static final int fragment1=0x7f090001;
+        public static final int action_settings=0x7f050009;
+        public static final int cb_switch1=0x7f050005;
+        public static final int cb_switch2=0x7f050006;
+        public static final int cb_switch3=0x7f050007;
+        public static final int cb_switch4=0x7f050008;
+        public static final int container=0x7f050003;
+        public static final int fragment1=0x7f050004;
+        /**  Menu item to cancel the current Activity 
+         */
+        public static final int twofortyfouram_locale_menu_dontsave=0x7f050001;
+        /**  Menu item to launch the Help 
+         */
+        public static final int twofortyfouram_locale_menu_help=0x7f050000;
+        /**  Menu item to save the current Activity 
+         */
+        public static final int twofortyfouram_locale_menu_save=0x7f050002;
+    }
+    public static final class integer {
+        /**  The maximum blurb length in characters.  This value is based on Latin characters. 
+         */
+        public static final int twofortyfouram_locale_maximum_blurb_length=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_switches=0x7f030001;
+        public static final int tasker_edit=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f0a0000;
+        public static final int twofortyfouram_locale_help_save_dontsave=0x7f0a0001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
-        public static final int switch1=0x7f060003;
-        public static final int switch2=0x7f060004;
-        public static final int switch3=0x7f060005;
-        public static final int switch4=0x7f060006;
+        public static final int action_settings=0x7f070007;
+        public static final int app_name=0x7f070005;
+        public static final int hello_world=0x7f070006;
+        public static final int message_hint=0x7f07000d;
+        public static final int plugin_name=0x7f07000c;
+        public static final int switch1=0x7f070008;
+        public static final int switch2=0x7f070009;
+        public static final int switch3=0x7f07000a;
+        public static final int switch4=0x7f07000b;
+        /**  A format string for breadcrumbs in the titlebar.  For left to right languages, such as English, the format will look something like "Main Activity > Sub Activity > Sub Activity 2".  Item #1 is the previous path, item #2 is the separator, and item #3 is the current item.  For most left to right languages, this shouldn't need to be changed. 
+         */
+        public static final int twofortyfouram_locale_breadcrumb_format=0x7f070000;
+        /**  The separator character for breadcrumbs.  For most left to right languages, this shouldn't need to be changed. 
+         */
+        public static final int twofortyfouram_locale_breadcrumb_separator=0x7f070001;
+        /**  The Don't Save button in the menu or Action Bar.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_dontsave=0x7f070002;
+        /**  The Help button in the menu or Action Bar.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_help=0x7f070003;
+        /**  The Save button in the menu or Action Bar.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_save=0x7f070004;
     }
     public static final class style {
         /** 
@@ -57,11 +93,23 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080004;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080005;
+        /**  Stupid Motorola.  This is a hack to avoid hideous ListViews on Motoblur devices.  http://community.developer.motorola.com/t5/MOTODEV-Blog/Why-Does-My-ListView-Look-Different/ba-p/17462 
+         */
+        public static final int MotorolaListViewHackStyle=0x7f080003;
+        /**  Dark style for all standard Locale and Locale plug-in Activity instances 
+         */
+        public static final int Theme_Locale_Dark=0x7f080000;
+        /**  Style for Activities that look like a dialog 
+         */
+        public static final int Theme_Locale_Dialog=0x7f080002;
+        /**  Light style for use within Locale.  This theme is usually not used.  The only place Locale uses this theme is to make dialogs look correct on Android 2.3 and earlier, because http://developer.android.com/reference/android/app/AlertDialog.Builder.html#getContext() requires Android 3.0. 
+         */
+        public static final int Theme_Locale_Light=0x7f080001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
---
+++ src/wrm/pihome/tasker/receiver/FireReceiver.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 two forty four a.m. LLC <http://www.twofortyfouram.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package wrm.pihome.tasker.receiver;
+
+import java.util.Locale;
+
+import wrm.pihome.tasker.bundle.BundleScrubber;
+import wrm.pihome.tasker.bundle.PluginBundleManager;
+import wrm.pihome.tasker.ui.EditActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * This is the "fire" BroadcastReceiver for a Locale Plug-in setting.
+ *
+ * @see com.twofortyfouram.locale.Intent#ACTION_FIRE_SETTING
+ * @see com.twofortyfouram.locale.Intent#EXTRA_BUNDLE
+ */
+public final class FireReceiver extends BroadcastReceiver
+{
+
+    /**
+     * @param context {@inheritDoc}.
+     * @param intent the incoming {@link com.twofortyfouram.locale.Intent#ACTION_FIRE_SETTING} Intent. This
+     *            should contain the {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} that was saved by
+     *            {@link EditActivity} and later broadcast by Locale.
+     */
+    @Override
+    public void onReceive(final Context context, final Intent intent)
+    {
+        /*
+         * Always be strict on input parameters! A malicious third-party app could send a malformed Intent.
+         */
+
+        if (!com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction()))
+        {
+                Log.e("pihome",
+                      String.format(Locale.US, "Received unexpected Intent action %s", intent.getAction())); //$NON-NLS-1$
+            return;
+        }
+
+        BundleScrubber.scrub(intent);
+
+        final Bundle bundle = intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+        BundleScrubber.scrub(bundle);
+
+        if (PluginBundleManager.isBundleValid(bundle))
+        {
+            final String message = bundle.getString(PluginBundleManager.BUNDLE_EXTRA_STRING_MESSAGE);
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    }
+}
